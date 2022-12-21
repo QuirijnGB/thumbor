6d9129df996f1ef1dca4c21a9c1416483918a9f3
danypata <dan.patacean@gmail.com>
2022-12-21 04:40:53 +0200

    upgraded supported flutter version, updated crypto library, fixed some linting issues (#3)

--- lib/src/thumbor_url.dart
+++ lib/src/thumbor_url.dart
@@ -2,85 +2,95 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 
-class TrimOrientation {
-  const TrimOrientation(String value) : value = value;
+enum TrimOrientation {
+  topLeft('top-left'),
+  bottomRight('bottom-right');
+
+  const TrimOrientation(this.value);
 
   final String value;
-
-  static const TrimOrientation topLeft = TrimOrientation("top-left");
-  static const TrimOrientation bottomRight = TrimOrientation("bottom-right");
 }
 
-class HorizontalAlignment {
-  const HorizontalAlignment(String value) : value = value;
+enum HorizontalAlignment {
+  left('left'),
+  center('center'),
+  right('right');
+
+  const HorizontalAlignment(this.value);
 
   final String value;
-
-  static const HorizontalAlignment left = HorizontalAlignment("left");
-  static const HorizontalAlignment center = HorizontalAlignment("center");
-  static const HorizontalAlignment right = HorizontalAlignment("right");
 }
 
-class VerticalAlignment {
-  const VerticalAlignment(String value) : value = value;
+enum VerticalAlignment {
+  top('top'),
+  middle('middle'),
+  bottom('bottom');
+
+  const VerticalAlignment(this.value);
 
   final String value;
-
-  static const VerticalAlignment top = VerticalAlignment("top");
-  static const VerticalAlignment middle = VerticalAlignment("middle");
-  static const VerticalAlignment bottom = VerticalAlignment("bottom");
 }
 
-class FitInStyle {
-  const FitInStyle(String value) : value = value;
+enum FitInStyle {
+  normal('fit-in'),
+  full('full-fit-in'),
+  adaptive('adaptive-fit-in');
+
+  const FitInStyle(this.value);
 
   final String value;
-
-  static const FitInStyle normal = FitInStyle("fit-in");
-  static const FitInStyle full = FitInStyle("full-fit-in");
-  static const FitInStyle adaptive = FitInStyle("adaptive-fit-in");
 }
 
-class ImageFormat {
-  const ImageFormat(String value) : value = value;
+enum ImageFormat {
+  gif('gif'),
+  jpeg('jpeg'),
+  png('png'),
+  webp('webp');
+
+  const ImageFormat(this.value);
 
   final String value;
-
-  static const ImageFormat GIF = ImageFormat("gif");
-  static const ImageFormat JPEG = ImageFormat("jpeg");
-  static const ImageFormat PNG = ImageFormat("png");
-  static const ImageFormat WEBP = ImageFormat("webp");
 }
 
 class ThumborUrl {
-  final String host;
-  final String key;
-  final String imageUrl;
-  static const PREFIX_UNSAFE = "unsafe/";
-  static const PREFIX_TRIM = "trim";
-  static const ORIGINAL_SIZE = -10000;
+  ThumborUrl({
+    required this.host,
+    this.key,
+    required this.imageUrl,
+  }) {
+    if (host.isEmpty) {
+      throw ArgumentError('Host may not be empty');
+    }
+  }
 
-  static const _FILTER_BRIGHTNESS = "brightness";
-  static const _FILTER_CONTRAST = "contrast";
-  static const _FILTER_NOISE = "noise";
-  static const _FILTER_QUALITY = "quality";
-  static const _FILTER_RGB = "rgb";
-  static const _FILTER_ROUND_CORNER = "round_corner";
-  static const _FILTER_WATERMARK = "watermark";
-  static const _FILTER_SHARPEN = "sharpen";
-  static const _FILTER_FILL = "fill";
-  static const _FILTER_FORMAT = "format";
-  static const _FILTER_FRAME = "frame";
-  static const _FILTER_STRIP_ICC = "strip_icc";
-  static const _FILTER_GRAYSCALE = "grayscale";
-  static const _FILTER_EQUALIZE = "equalize";
-  static const _FILTER_BLUR = "blur";
-  static const _FILTER_NO_UPSCALE = "no_upscale";
-  static const _FILTER_ROTATE = "rotate";
+  final String host;
+  final String? key;
+  final String imageUrl;
+  static const _prefixUnsafe = 'unsafe/';
+  static const _prefixTrim = 'trim';
+  static const originalSize = -10000;
+
+  static const _filterBrightness = 'brightness';
+  static const _filterContrast = 'contrast';
+  static const _filterNoise = 'noise';
+  static const _filterQuality = 'quality';
+  static const _filterRGB = 'rgb';
+  static const _filterRoundCorner = 'round_corner';
+  static const _filterWatermark = 'watermark';
+  static const _filterSharpen = 'sharpen';
+  static const _filterFill = 'fill';
+  static const _filterFormat = 'format';
+  static const _filterFrame = 'frame';
+  static const _filterStripICC = 'strip_icc';
+  static const _filterGrayscale = 'grayscale';
+  static const _filterEqualize = 'equalize';
+  static const _filterBlur = 'blur';
+  static const _filterNoUpscale = 'no_upscale';
+  static const _filterRotate = 'rotate';
 
   bool _hasTrim = false;
-  TrimOrientation _trimOrientation;
-  int _trimTolerance;
+  TrimOrientation? _trimOrientation;
+  late int _trimTolerance;
 
   bool _hasCrop = false;
   int _cropTop = 0;
@@ -89,360 +99,335 @@ class ThumborUrl {
   int _cropRight = 0;
 
   bool _hasResize = false;
-  int _resizeHeight;
-  int _resizeWidth;
+  int? _resizeHeight;
+  int? _resizeWidth;
 
   bool _flipVertically = false;
   bool _flipHorizontally = false;
   bool _smart = false;
 
-  HorizontalAlignment _horizontalAlign;
-  VerticalAlignment _verticalAlign;
+  HorizontalAlignment? _horizontalAlign;
+  VerticalAlignment? _verticalAlign;
 
-  FitInStyle _fitInStyle;
+  FitInStyle? _fitInStyle;
 
-  List<String> _filters;
+  final List<String> _filters = [];
 
-  ThumborUrl({
-    this.host,
-    this.key,
-    this.imageUrl,
-  })  : assert(host != null),
-        assert(imageUrl != null) {
-    if (host.isEmpty) {
-      throw ArgumentError("Host may not be empty");
-    }
-  }
-
-  void trim({TrimOrientation orientation, int tolerance = 0}) {
-    this._hasTrim = true;
-    this._trimOrientation = orientation;
-    this._trimTolerance = tolerance;
+  void trim({TrimOrientation? orientation, int tolerance = 0}) {
+    _hasTrim = true;
+    _trimOrientation = orientation;
+    _trimTolerance = tolerance;
   }
 
   void crop(int top, int left, int bottom, int right) {
     if (top < 0) {
-      throw ArgumentError("Top must be greater or equal to zero");
+      throw ArgumentError('Top must be greater or equal to zero');
     }
     if (left < 0) {
-      throw ArgumentError("Left must be greater or equal to zero");
+      throw ArgumentError('Left must be greater or equal to zero');
     }
     if (bottom < 0) {
-      throw ArgumentError("Bottom must be greater or equal to zero");
+      throw ArgumentError('Bottom must be greater or equal to zero');
     }
     if (right < 0) {
-      throw ArgumentError("Right must be greater or equal to zero");
+      throw ArgumentError('Right must be greater or equal to zero');
     }
 
-    this._hasCrop = true;
-    this._cropTop = top;
-    this._cropLeft = left;
-    this._cropBottom = bottom;
-    this._cropRight = right;
+    _hasCrop = true;
+    _cropTop = top;
+    _cropLeft = left;
+    _cropBottom = bottom;
+    _cropRight = right;
   }
 
   void resize({int width = 0, int height = 0}) {
-    if (width < 0 && width != ORIGINAL_SIZE) {
-      throw ArgumentError("Width must be a positive number.");
+    if (width < 0 && width != originalSize) {
+      throw ArgumentError('Width must be a positive number.');
     }
-    if (height < 0 && height != ORIGINAL_SIZE) {
-      throw ArgumentError("Height must be a positive number.");
+    if (height < 0 && height != originalSize) {
+      throw ArgumentError('Height must be a positive number.');
     }
     if (width == 0 && height == 0) {
-      throw ArgumentError("Both width and height must not be zero.");
+      throw ArgumentError('Both width and height must not be zero.');
     }
 
-    this._hasResize = true;
+    _hasResize = true;
 
-    this._resizeHeight = height;
-    this._resizeWidth = width;
+    _resizeHeight = height;
+    _resizeWidth = width;
   }
 
   void flipVertically() {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
-    this._flipVertically = true;
+    _flipVertically = true;
   }
 
   void flipHorizontally() {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
-    this._flipHorizontally = true;
+    _flipHorizontally = true;
   }
 
   void smart() {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
-    this._smart = true;
+    _smart = true;
   }
 
   void horizontalAlign(HorizontalAlignment align) {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
 
-    this._horizontalAlign = align;
+    _horizontalAlign = align;
   }
 
   void verticalAlign(VerticalAlignment align) {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
 
-    this._verticalAlign = align;
+    _verticalAlign = align;
   }
 
   void fitIn(FitInStyle style) {
-    if (!this._hasResize) {
-      throw StateError("Resize must be called first");
+    if (!_hasResize) {
+      throw StateError('Resize must be called first');
     }
 
-    this._fitInStyle = style;
+    _fitInStyle = style;
   }
 
   void filter(List<String> filters) {
-    if (filters.isEmpty) {
-      throw ArgumentError("You must provide at least one filter.");
-    }
-    if (this._filters == null) {
-      this._filters = List();
-    }
-
-    filters.forEach((filter) {
-      if (filter == null || filter.isEmpty) {
-        throw ArgumentError("Filter must not be blank.");
-      }
-      this._filters.add(filter);
-    });
+    _filters.addAll(filters.where((element) => element.isNotEmpty));
   }
 
   String toUrl() {
-    return key == null ? toUnsafeUrl() : toSafeUrl();
+    if (key != null) {
+      return toSafeUrl();
+    } else {
+      return toUnsafeUrl();
+    }
   }
 
   String toUnsafeUrl() {
-    return host + PREFIX_UNSAFE + _assembleConfig();
+    return host + _prefixUnsafe + _assembleConfig();
   }
 
   String toSafeUrl() {
-    if (key == null) {
-      throw StateError("Cannot build safe URL without a key.");
-    }
+    final config = _assembleConfig();
 
-    String config = _assembleConfig();
-
-    var hmac = Hmac(sha1, utf8.encode(key));
-    var digest = hmac.convert(utf8.encode(config));
-    var encoded = base64Url.encode(digest.bytes);
-    return host + "$encoded/" + config;
+    final hmac = Hmac(sha1, utf8.encode(key!));
+    final digest = hmac.convert(utf8.encode(config));
+    final encoded = base64Url.encode(digest.bytes);
+    return '$host$encoded/$config';
   }
 
   String _assembleConfig() {
-    String config = "";
+    var config = '';
 
     if (_hasTrim) {
-      config += PREFIX_TRIM;
+      config += _prefixTrim;
 
       if (_trimOrientation != null) {
-        config += ":${_trimOrientation.value}";
+        config += ':${_trimOrientation?.value ?? ''}';
         if (_trimTolerance > 0) {
-          config += ":${_trimTolerance.toString()}";
+          config += ':${_trimTolerance.toString()}';
         }
       }
-      config += "/";
+      config += '/';
     }
 
     if (_hasCrop) {
-      config += "${_cropLeft}x${_cropTop}:${_cropRight}x${_cropBottom}/";
+      config += '${_cropLeft}x$_cropTop:${_cropRight}x$_cropBottom/';
     }
 
     if (_hasResize) {
       if (_fitInStyle != null) {
-        config += "${_fitInStyle.value}/";
+        config += '${_fitInStyle?.value}/';
       }
 
       if (_flipHorizontally) {
-        config += "-";
+        config += '-';
       }
-      if (_resizeWidth == ORIGINAL_SIZE) {
-        config += "orig";
+      if (_resizeWidth == originalSize) {
+        config += 'orig';
       } else {
-        config += "${_resizeWidth}";
+        config += '$_resizeWidth';
       }
 
-      config += "x";
+      config += 'x';
 
       if (_flipVertically) {
-        config += "-";
+        config += '-';
       }
-      if (_resizeHeight == ORIGINAL_SIZE) {
-        config += "orig";
+      if (_resizeHeight == originalSize) {
+        config += 'orig';
       } else {
-        config += "${_resizeHeight}";
+        config += '$_resizeHeight';
       }
 
       if (_smart) {
-        config += "/smart";
+        config += '/smart';
       } else {
         if (_horizontalAlign != null) {
-          config += "/${_horizontalAlign.value}";
+          config += '/${_horizontalAlign!.value}';
         }
         if (_verticalAlign != null) {
-          config += "/${_verticalAlign.value}";
+          config += '/${_verticalAlign!.value}';
         }
       }
 
-      config += "/";
+      config += '/';
+    }
+    if (_filters.isNotEmpty) {
+      config += 'filters:';
+
+      config += _filters.join(':');
+      config += '/';
     }
 
-    if (_filters != null) {
-      config += "filters";
-
-      _filters.forEach((filter) {
-        config += ":$filter";
-      });
-      config += "/";
-    }
-
-    config += imageUrl;
-    return config;
+    return config + imageUrl;
   }
 
   static String brightness(int amount) {
     if (amount < -100 || amount > 100) {
-      throw ArgumentError("Amount must be between -100 and 100, inclusive.");
+      throw ArgumentError('Amount must be between -100 and 100, inclusive.');
     }
-    return "$_FILTER_BRIGHTNESS($amount)";
+    return '$_filterBrightness($amount)';
   }
 
   static String contrast(int amount) {
     if (amount < -100 || amount > 100) {
-      throw ArgumentError("Amount must be between -100 and 100, inclusive.");
+      throw ArgumentError('Amount must be between -100 and 100, inclusive.');
     }
-    return "$_FILTER_CONTRAST($amount)";
+    return '$_filterContrast($amount)';
   }
 
   static String noise(int amount) {
     if (amount < 0 || amount > 100) {
-      throw ArgumentError("Amount must be between 0 and 100, inclusive");
+      throw ArgumentError('Amount must be between 0 and 100, inclusive');
     }
-    return "$_FILTER_NOISE($amount)";
+    return '$_filterNoise($amount)';
   }
 
   static String quality(int amount) {
     if (amount < 0 || amount > 100) {
-      throw ArgumentError("Amount must be between 0 and 100, inclusive.");
+      throw ArgumentError('Amount must be between 0 and 100, inclusive.');
     }
-    return "$_FILTER_QUALITY($amount)";
+    return '$_filterQuality($amount)';
   }
 
   static String rgb(int r, int g, int b) {
     if (r < -100 || r > 100) {
-      throw ArgumentError("Red value must be between -100 and 100, inclusive.");
+      throw ArgumentError('Red value must be between -100 and 100, inclusive.');
     }
     if (g < -100 || g > 100) {
       throw ArgumentError(
-          "Green value must be between -100 and 100, inclusive.");
+        'Green value must be between -100 and 100, inclusive.',
+      );
     }
     if (b < -100 || b > 100) {
       throw ArgumentError(
-          "Blue value must be between -100 and 100, inclusive.");
+        'Blue value must be between -100 and 100, inclusive.',
+      );
     }
-    return "$_FILTER_RGB($r,$g,$b)";
+    return '$_filterRGB($r,$g,$b)';
   }
 
   static String roundCorner(int radiusInner, int radiusOuter, int color) {
     if (radiusInner < 1) {
-      throw ArgumentError("Radius must be greater than zero.");
+      throw ArgumentError('Radius must be greater than zero.');
     }
     if (radiusOuter < 0) {
       throw ArgumentError(
-          "Outer radius must be greater than or equal to zero.");
+        'Outer radius must be greater than or equal to zero.',
+      );
     }
 
-    String builder = "$_FILTER_ROUND_CORNER($radiusInner";
+    var builder = '$_filterRoundCorner($radiusInner';
 
     if (radiusOuter > 0) {
-      builder += "|";
-      builder += "$radiusOuter";
+      builder += '|';
+      builder += '$radiusOuter';
     }
-    final int r = (color & 0xFF0000) >> 16;
-    final int g = (color & 0xFF00) >> 8;
-    final int b = color & 0xFF;
+    final r = (color & 0xFF0000) >> 16;
+    final g = (color & 0xFF00) >> 8;
+    final b = color & 0xFF;
 
-    builder += ",$r,$g,$b)";
-
-    return builder;
+    return builder += ',$r,$g,$b)';
   }
 
   static String watermark(String imageUrl, int x, int y, int transparency) {
-    if (imageUrl == null || imageUrl.isEmpty) {
-      throw ArgumentError("Image URL must not be blank.");
+    if (imageUrl.isEmpty) {
+      throw ArgumentError('Image URL must not be blank.');
     }
     if (transparency < 0 || transparency > 100) {
-      throw ArgumentError("Transparency must be between 0 and 100, inclusive.");
+      throw ArgumentError('Transparency must be between 0 and 100, inclusive.');
     }
-    return "$_FILTER_WATERMARK($imageUrl,$x,$y,$transparency)";
+    return '$_filterWatermark($imageUrl,$x,$y,$transparency)';
   }
 
-  static String sharpen(double amount, double radius, bool luminanceOnly) {
-    return "$_FILTER_SHARPEN($amount,$radius,$luminanceOnly)";
+  static String sharpenLuminacenceOnly(double amount, double radius) {
+    return '$_filterSharpen($amount,$radius,true)';
+  }
+
+  static String sharpen(double amount, double radius) {
+    return '$_filterSharpen($amount,$radius,false)';
   }
 
   static String fill(int color) {
-    return "$_FILTER_FILL(${color.toRadixString(16)})";
+    return '$_filterFill(${color.toRadixString(16)})';
   }
 
   static String format(ImageFormat format) {
-    if (format == null) {
-      throw ArgumentError("You must specify an image format.");
-    }
-    return "$_FILTER_FORMAT(${format.value})";
+    return '$_filterFormat(${format.value})';
   }
 
   static String frame(String imageUrl) {
-    if (imageUrl == null || imageUrl.isEmpty) {
-      throw ArgumentError("Image URL must not be blank.");
+    if (imageUrl.isEmpty) {
+      throw ArgumentError('Image URL must not be blank.');
     }
-    return "$_FILTER_FRAME($imageUrl)";
+    return '$_filterFrame($imageUrl)';
   }
 
   static String stripicc() {
-    return "$_FILTER_STRIP_ICC()";
+    return '$_filterStripICC()';
   }
 
   static String grayscale() {
-    return "$_FILTER_GRAYSCALE()";
+    return '$_filterGrayscale()';
   }
 
   static String equalize() {
-    return "$_FILTER_EQUALIZE()";
+    return '$_filterEqualize()';
   }
 
   static String blur(int radius, int sigma) {
     if (radius < 1) {
-      throw ArgumentError("Radius must be greater than zero.");
+      throw ArgumentError('Radius must be greater than zero.');
     }
     if (radius > 150) {
-      throw ArgumentError("Radius must be lower or equal than 150.");
+      throw ArgumentError('Radius must be lower or equal than 150.');
     }
     if (sigma < 0) {
-      throw ArgumentError("Sigma must be greater than zero.");
+      throw ArgumentError('Sigma must be greater than zero.');
     }
-    return "$_FILTER_BLUR($radius,$sigma)";
+    return '$_filterBlur($radius,$sigma)';
   }
 
   static String noUpscale() {
-    return "$_FILTER_NO_UPSCALE()";
+    return '$_filterNoUpscale()';
   }
 
   static String rotate(int angle) {
     if (angle % 90 != 0) {
-      throw ArgumentError("Angle must be multiple of 90°");
+      throw ArgumentError('Angle must be multiple of 90°');
     }
-    return "$_FILTER_ROTATE($angle)";
+    return '$_filterRotate($angle)';
   }
 }
--- test/src/thumbor_test.dart
+++ test/src/thumbor_test.dart
@@ -1,17 +1,19 @@
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:thumbor/thumbor.dart';
 
 void main() {
   test('creates an instance of Thumbor with just the hostname', () {
-    var thumbor = new Thumbor(host: "hostname");
+    final thumbor = Thumbor(
+      host: 'hostname',
+    );
 
-    expect(thumbor.host, "hostname");
+    expect(thumbor.host, 'hostname');
     expect(thumbor.key, isNull);
   });
   test('creates an instance of Thumbor with the hostname and a key', () {
-    var thumbor = new Thumbor(host: "hostname", key: "4s3cr37k3y");
+    final thumbor = Thumbor(host: 'hostname', key: '4s3cr37k3y');
 
-    expect(thumbor.host, "hostname");
-    expect(thumbor.key, "4s3cr37k3y");
+    expect(thumbor.host, 'hostname');
+    expect(thumbor.key, '4s3cr37k3y');
   });
 }
